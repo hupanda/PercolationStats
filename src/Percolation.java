a12f6e14153451f6be94ccef208d702985ac91bf
hupanda <pandahu@Panda-Hus-iMac.local>
2014-07-29 20:32:21 +0800

    Final Submission

--- src/Percolation.java
+++ src/Percolation.java
@@ -4,36 +4,85 @@
  * The constructor should take time proportional to N^2; 
  * all methods should take constant time plus a constant number of calls to the union-find methods union(), find(), connected(), and count().
  */
-public class Percolation 
-{
-	
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N)
-	{
-		return;
-   	}
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j)         
-	{
-		return;
-	}
+import java.util.Arrays;
+
+public class Percolation {
+        private static int CLOSED = -1;
+        private int[] matrix;
+        private int dimension;
+        private boolean percolates;
+        
+        // create N-by-N grid, with all sites blocked
+        public Percolation(int N)
+        {
+                if (N <= 0) throw new IllegalArgumentException();
+                matrix = new int[N*N];
+                Arrays.fill(matrix, CLOSED);
+                dimension = N;
+                return;
+           }
+        
+        // open site (row i, column j) if it is not already
+        public void open(int i, int j)         
+        {        
+                if (i < 1 || j < 1 || i > dimension || j > dimension) throw new IndexOutOfBoundsException();
+                if (isOpen(i, j)) return;
+                int index = getIndex(i, j);
+                matrix[index] = index;
+                if (i > 2 && isOpen(i-1, j)) union(index, getIndex(i-1, j));
+                if (i < dimension && isOpen(i+1, j)) union(index, getIndex(i+1, j));
+                if (j > 2 && isOpen(i, j-1)) union(index, getIndex(i, j-1));
+                if (j < dimension && isOpen(i, j+1)) union(index, getIndex(i, j+1));
+        }
    
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j)    
-   	{
-	   	return true;
-   	}
-	
-	// is site (row i, column j) full?
-	public boolean isFull(int i, int j)    
-	{
-		return true;
-	}
-   
-	// does the system percolate?
-	public boolean percolates()            
-	{
-		return true;
-	}
-	
+        // is site (row i, column j) open?
+        public boolean isOpen(int i, int j)    
+           {
+                if (i < 1 || j < 1 || i > dimension || j > dimension) throw new IndexOutOfBoundsException();
+                return !(matrix[getIndex(i, j)] == CLOSED);
+           }
+        
+        // is site (row i, column j) full?
+        public boolean isFull(int i, int j)    
+        {
+                if (i < 1 || j < 1 || i > dimension || j > dimension) throw new IndexOutOfBoundsException();
+                return isOpen(i, j) && root(getIndex(i, j)) < dimension;
+        }
+    
+        // does the system percolate?
+        public boolean percolates()            
+        {
+                return percolates;
+        }
+        
+        private void union(int p, int q)
+        {
+                int i = root(p);
+                int j = root(q);
+                int min = Math.min(i, j);
+                matrix[j] = min;
+                matrix[i] = min;
+                if (min < dimension)
+                {
+                        if (isLastRow(p) || isLastRow(q)) percolates = true;
+                }
+        }
+        
+        private int root(int j)
+        {
+                int i = j;
+                while (i != matrix[i]) i = matrix[i];
+                return i;
+        }
+        
+        private int getIndex(int i, int j)
+        {
+                return (i-1) * dimension + j - 1;
+        }
+        
+        private boolean isLastRow(int i)
+        {
+                return i >= dimension * (dimension - 1); 
+        }
+        
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,68 +1,102 @@
-//The constructor should throw a java.lang.IllegalArgumentException if either N ² 0 or T ² 0.
-
-public class PercolationStats 
-{
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T)    
-	{
-		if(N <= 0 || T <=0) throw new java.lang.IllegalArgumentException();
-	}
-	
-	// test client, described below
-	public static void main(String[] args)
-	{
-		if(args.length != 2) 
-		{
-			System.out.println("Incorrect number of arguments input!");
-			return;
-		}
-		
-		try
-		{
-			int N = Integer.parseInt(args[0]);
-			int T = Integer.parseInt(args[1]);
-			PercolationStats stats = new PercolationStats(N, T);
-			System.out.println(stats.mean());
-			System.out.println(stats.stddev());
-			System.out.println(stats.confidenceLo() + ", " + stats.confidenceHi());
-		}
-		catch(NumberFormatException e)
-		{
-			System.out.println("Incorrect format of arguments input!");
-		}
-		catch(IllegalArgumentException e)
-		{
-			System.out.println("Illegal arguments input!");
-		}
-		catch(Exception e)
-		{
-			System.out.println("Error occurs!");
-		}
-		
-	}
-	
-	// sample mean of percolation threshold
-	public double mean()             
-	{
-		return 0;
-	}
-	
-	// sample standard deviation of percolation threshold
-	public double stddev()                 
-	{
-		return 0;
-	}
-	
-	// returns lower bound of the 95% confidence interval
-	public double confidenceLo()             
-	{
-		return 0;
-	}
-	
-	// returns upper bound of the 95% confidence interval
-	public double confidenceHi()             
-	{
-		return 0;
-	}
+public class PercolationStats {
+        private double mean;
+        private double stddev;
+        private double confidenceLo;
+        private double confidenceHi;
+        
+        // perform T independent computational experiments on an N-by-N grid
+        public PercolationStats(int N, int T)    
+        {
+                if (N <= 0 || T <= 0) throw new IllegalArgumentException();
+                
+                double sum = 0;
+                double squaredSum = 0;
+                java.util.Random rand = new java.util.Random();
+                
+                for (int i = 0; i < T; i++)
+                {
+                        Percolation p = new Percolation(N);
+                        double n = 0;
+                        int row;
+                        int col;
+                        while (!p.percolates())
+                        {
+                                do
+                                {
+                                        row = rand.nextInt(N) + 1;
+                                        col = rand.nextInt(N) + 1;
+                                }
+                                while (p.isOpen(row, col));
+                                
+                                p.open(row, col);
+                                n++;
+                        }
+                        
+                        double result = n / N / N;
+                        sum += result;
+                        squaredSum += result * result; 
+                        
+                }
+                
+                //Calculate the stats
+                mean = sum / T;
+                stddev = Math.sqrt((squaredSum - 2 * mean * sum + T * mean * mean) / (T - 1));
+                double temp = 1.96 * stddev / Math.sqrt(T);
+                confidenceLo = mean - temp;
+                confidenceHi = mean + temp;
+        }
+        
+        // test client, described below
+        public static void main(String[] args)
+        {
+                if (args.length != 2) 
+                {
+                        System.out.println("Incorrect number of arguments input!");
+                        return;
+                }
+                
+                try
+                {
+                        int N = Integer.parseInt(args[0]);
+                        int T = Integer.parseInt(args[1]);
+                        PercolationStats stats = new PercolationStats(N, T);
+                        System.out.println("mean                    = " + stats.mean());
+                        System.out.println("stddev                  = " + stats.stddev());
+                        System.out.println("95% confidence interval = " + stats.confidenceLo() + ", " + stats.confidenceHi());
+                }
+                catch (NumberFormatException e)
+                {
+                        System.out.println("Incorrect format of arguments input!");
+                }
+                catch (IllegalArgumentException e)
+                {
+                        System.out.println("Illegal arguments input!");
+                }
+                
+        }
+        
+        // sample mean of percolation threshold
+        public double mean() 
+        {
+                return mean;
+        }
+        
+        // sample standard deviation of percolation threshold
+        public double stddev()                 
+        {
+                return stddev;
+        }
+        
+        // returns lower bound of the 95% confidence interval
+        public double confidenceLo()             
+        {
+                return confidenceLo;
+        }
+        
+        // returns upper bound of the 95% confidence interval
+        public double confidenceHi()             
+        {
+                return confidenceHi;
+        }
 
 }
